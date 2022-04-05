84a8f6122381e8d7aa08dd32f5c120c22fa01c91
mingh <minghao_Cheng@aliyun.com>
2022-04-05 19:10:35 +0800

    搜集数据

--- ControllerData.h
+++ ControllerData.h
@@ -7,18 +7,18 @@ struct ControllerData
 {
     Mh::MhIndustrialSCARA robotscara;
     PLCOpenMotion motor;
-    ControllerData()
-    {
-        for (int i = 0; i < 4; i++)
-        {
-            motor.Mc_AddAxisToGroup(0, i, TRUE, i);
-        }
-        MC_KIN_REF Type;
+    // ControllerData()
+    // {
+    //     for (int i = 0; i < 4; i++)
+    //     {
+    //         motor.Mc_AddAxisToGroup(0, i, TRUE, i);
+    //     }
+    //     MC_KIN_REF Type;
         
-        Type.Device=DeviceType_ScaraJointCsp;
-        motor.MC_SetKinTransform(0,&Type,mcImmediately,sizeof(Type));
-        motor.MC_GroupEnable(0,true);
-    }
+    //     Type.Device=DeviceType_ScaraJointCsp;
+    //     motor.MC_SetKinTransform(0,&Type,mcImmediately,sizeof(Type));
+    //     motor.MC_GroupEnable(0,true);
+    // }
 };
 
 extern ControllerData controllerdata;
